bad8caea890b1b6f01a1874b7dd11f914e83198b
Erick <erick_malvaez_c@encrym.edu.mx>
2024-02-07 16:44:25 -0600

    alerta para cuando te logeas

--- src/pag/incio.svelte
+++ src/pag/incio.svelte
@@ -14,91 +14,105 @@
     let mes = currentTime.getMonth() + 1;
     let anno = currentTime.getUTCFullYear();
 
-  console.log(hora+":"+minuto)
+    let counter = 0
+    const incr = () => (counter += 1)
 
-  function volver(){
-    tusuario=3
-    barras_correo=true
-  }
+    let clear
+    $: {
+      clearInterval(clear)
+      clear = setInterval(incr, 1000)
+      if(counter==60){
+        if(minuto<59)
+          minuto += 1
+        else{
+          minuto=0
+          if(hora<23)
+            hora += 1
+          else
+            hora = 0
+        }
+      counter=0
+      }
+    }
+
   function barraCodigo(){
     if(barras_correo)
       barras_correo=false
     else
       barras_correo=true
+    codigo_correo=null
+    contrasena=null
+  }
+
+  let codigo_correo
+  let contrasena
+  let lanzar_modal=false
+
+  function verifica(){
+    if(codigo_correo == '' || codigo_correo == null || codigo_correo == ' ')
+      alert("No hay datos")
+    else
+      if(barras_correo)
+        alert("Hora Registrada a las: "+hora+":"+minuto)
+
+      else
+        if(contrasena=='' || contrasena==null || contrasena==' ')
+          alert("Falta Contraseña")
+        else
+          alert("Hora Registrada a las: "+hora+":"+minuto)
   }
 </script>
 
 <main>
-    <div class="card bienvenida" style="padding: 0%;">
-      <div class="row" style="max-width: 100%; max-height: 100%;">
-        <div class="col-5 mb-3" >
-          <img class="img1" src={imagen[tusuario]} alt="">
-        </div>
-        <div class="col-7 mb-3" >
-          <div class="row" style="background-color: white; border:1px white solid; border-radius:0 30px 0 0;">
-            <div class="col-4">
-              <img class="img2" src="https://latinrev.flacso.org.ar/sites/default/files/logos/logo_encrym-01_2_-_coordinacion_editorial_intervencion.png" alt="">
-            </div>
-            <div class="col-1">
-              <br>
-              <h2 style="color: #FB7D0C;">|</h2>
-            </div>
-            <div class="col-6">
-              <br>
-              <h2>{titulo[tusuario]}</h2>
-            </div>
+  <div class="card bienvenida" style="padding: 0%;">
+    <div class="row" style="max-width: 100%; max-height: 100%;">
+      <div class="col-5 mb-3" >
+        <img class="img1" src={imagen[tusuario]} alt="">
+      </div>
+      <div class="col-7 mb-3" >
+        <div class="row" style="background-color: white; border:1px white solid; border-radius:0 30px 0 0;">
+          <div class="col-4">
+            <img class="img2" src="https://latinrev.flacso.org.ar/sites/default/files/logos/logo_encrym-01_2_-_coordinacion_editorial_intervencion.png" alt="">
           </div>
-          <div class="row" style="background-color: #F5F5F5;">
-            <div class="col-12"><br></div>
-            <div class="col-12"><h2>{tusuario==0 ? "Registro de Horas":"Inicio de Sesión"}</h2></div>
-            <div class="col-12"><h5>Fecha: {dia+"/"+mes+"/"+anno}<br>Son las {hora+" horas con "+minuto+" minutos"}</h5></div>
-            <!-- <div class="col-12"><br></div> -->
-            <div class="col-12"><br></div>
-            <div class="col-12"><span>{barras_correo ? "Escanee Su Código de Barras:":"Ingrese Su Correo:"}</span><input type={barras_correo ? "text":"text"} autofocus autocomplete="off"></div>
-            <div class="col-12"><br></div>
-            <div class="col-12">
-              {#if barras_correo==false}
-                <span>Contraseña:</span><br>
-                <input type="password" id="contrasenna" autocomplete="off"><br><br>
-              {/if}
-              <button style="border: none; color:#FB7D0C; background-color: #F5F5F5;  text-decoration:double" on:click={barraCodigo}>{barras_correo ? "Utilizar Correo Electronico":"Utilizar Codigo de Barras"}</button>
-            </div>
-            <!-- <div class="col-12"><br></div> -->
-            <div class="col-12"><br></div>
-            <div class="col-12">
-              <button style="background-color: #FB7D0C; color: white; pading: 20px; font-size:20px"><a href={tusuario==0 ? "/PagUsuarios":"/PagRHumanos"} use:link>Iniciar Sesión</a></button>
-            </div>
+          <div class="col-1">
+            <br>
+            <h2 style="color: #FB7D0C;">|</h2>
+          </div>
+          <div class="col-6">
+            <br>
+            <h2>{titulo[tusuario]}</h2>
+          </div>
+        </div>
+        <div class="row" style="background-color: #F5F5F5;">
+          <div class="col-12"><br></div>
+          <div class="col-12"><h2>{tusuario==0 ? "Registro de Horas":"Inicio de Sesión"}</h2></div>
+          <div class="col-12"><h5>Fecha: {dia+"/"+mes+"/"+anno}<br>Son las {hora+" horas con "+minuto+" minutos"}</h5></div>
+          <div class="col-12"><br></div>
+          <div class="col-12"><span>{barras_correo ? "Escanee Su Código de Barras:":"Ingrese Su Correo:"}</span><input type="text" autofocus autocomplete="off" bind:value={codigo_correo}></div>
+          <div class="col-12"><br></div>
+          <div class="col-12">
+            {#if barras_correo==false}
+              <span>Contraseña:</span><br>
+              <input type="password" id="contrasenna" autocomplete="off" bind:value={contrasena}><br><br>
+            {/if}
+            <button style="border: none; color:#FB7D0C; background-color: #F5F5F5;  text-decoration:double" on:click={barraCodigo}>{barras_correo ? "Utilizar Correo Electronico":"Utilizar Codigo de Barras"}</button>
+          </div>
+          <div class="col-12"><br></div>
+          <div class="col-12">
+            <button style="background-color: #FB7D0C; color: white; pading: 20px; font-size:20px" on:click={()=>verifica()}>Iniciar Sesión</button>
           </div>
         </div>
       </div>
     </div>
-
+  </div>
 </main>
 
 <style>
-  #tipousuario{
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    background-color:#FB7D0C;
-    color: white;
-    border-radius: 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-wrap: wrap;
-  }
 
   button{
     border-radius: 5px;
     border: 0;
     margin-right: 10px;
-  }
-
-  .btnSeleccion{
-    background: none;
-    border-radius: 100%;
-    border: 5px!important;
   }
 
   input{
@@ -139,10 +153,5 @@
 
   h5{
     color: #7D7770;
-  }
-
-  a{
-    text-decoration: none;
-    color: white;
   }
 </style>
